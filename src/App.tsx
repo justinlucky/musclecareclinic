1fca32b8976f49cd0cedc37e8ae9da5f937c20d6
Justin Lucky <lungchuingamriammei@gmail.com>
2025-01-03 00:39:38 -0800

    Hello Changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Services from './pages/Services'
-import PatientEducation from './pages/PatientEducation'
 import Contact from './pages/Contact'
 import ServiceDetails from './pages/ServiceDetails'
 import BookForm from './pages/BookForm'
@@ -20,7 +19,6 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/about/gallery" element={<Gallery/>} />
           <Route path="/services" element={<Services />} />
-          <Route path="/education" element={<PatientEducation />} />
           <Route path="/contact" element={<Contact />} /> 
           <Route path="/service/:id" element={<ServiceDetails />} />
           <Route path="/service/booking/:id" element={<BookForm />} />
