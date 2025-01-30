7281dfc343f65e3cca810c6990b51df9e20d9d7e
Justin Lucky <lungchuingamriammei@gmail.com>
2025-01-30 11:16:04 -0800

    hmm

--- src/pages/TeamContact.tsx
+++ src/pages/TeamContact.tsx
@@ -7,7 +7,7 @@ const TeamContact: React.FC = () => {
   const title = queryParams.get("title");
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-secondary">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-secondary px-5">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
         <h1 className="text-3xl font-bold mb-4 text-text">Contact {name}</h1>
         <p className="text-gray-700 mb-4">{title}</p>
